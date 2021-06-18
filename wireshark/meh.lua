9bbb38b704ff1d914ab35735971015a5599cac0b
Rajiv Kushwaha <raj25by10@gmail.com>
2021-06-18 16:20:31 +0530

    Dissector: Add meh protocol message format

--- wireshark/meh.lua
+++ wireshark/meh.lua
@@ -1,13 +1,42 @@
 meh_protocol = Proto("Meh",  "Meh Protocol")
 
+--[[
+
+meh message format:
+-------------------
+
+        0         4         8                16
+        +---------+---------+----------------+
+        |d|  ver  | flags   |    opcode      |
+        +---------+--------------------------+
+        |                length              |
+        +------------------------------------+
+        |                                    |
+        .            ...  body ...           .
+        .                                    .
+        +-------------------------------------
+
+    d (1 bit): direction of message
+               1: client to server
+               0: server to client
+
+    ver(3 bit): protocol version
+    flags(4 bit)
+    opcode(1 byte)
+    length(2 bytes): length of message payload following the header
+    body (variable): message payload
+--]]
+
 local t_direction = {
     [0] = "ServerToClient",
     [1] = "ClientToServer"
 }
+
 local t_opcode = {
     [0] = "Invalid",
     [1] = "Handshake",
 }
+
 message_direction = ProtoField.uint8("meh.direction", "Direction", base.DEC, t_direction, 0x80)
 message_version = ProtoField.uint8("meh.version", "Version", base.DEC, NULL, 0x70)
 message_flags = ProtoField.uint8("meh.flags", "Flags", base.DEC, NULL, 0x0F)
@@ -26,7 +55,7 @@ function meh_protocol.dissector(buffer, pinfo, tree)
   subtree:add(message_direction, buffer(0, 1))
   subtree:add(message_version, buffer(0, 1))
   subtree:add(message_flags, buffer(0, 1))
-  subtree:add_le(message_opcode, buffer(1, 1))
+  subtree:add(message_opcode, buffer(1, 1))
   subtree:add(message_length, buffer(2, 2))
 end
 
